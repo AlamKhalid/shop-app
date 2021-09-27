d27a0af0cf1d32cf1ad87f5aa2c41bfb24ef9f7f
Alam <alamkhalid0803@gmail.com>
2021-09-27 23:10:54 +0500

    provider and favorites

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './product_item.dart';
+import '../providers/product.dart';
+import '../providers/products.dart';
+
+class ProductsGrid extends StatelessWidget {
+  final bool showFavorites;
+
+  ProductsGrid(this.showFavorites);
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products =
+        showFavorites ? productsData.favoriteItems : productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (context, idx) => ChangeNotifierProvider.value(
+        value: products[idx],
+        child: ProductItem(),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
